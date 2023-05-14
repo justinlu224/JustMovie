f97eae224ec62964aa2f06ba0b3f55e091413675
justin045 <justinlu045@gmail.com>
2023-05-14 13:56:00 +0800

    refactor: ResultStatus

--- app/src/main/java/com/justin/justmovie/model/ResultStatus.kt
+++ app/src/main/java/com/justin/justmovie/model/ResultStatus.kt
@@ -3,5 +3,5 @@ package com.justin.justmovie.model
 sealed interface ResultStatus<T>
 
 data class Success<T>(val data:T):ResultStatus<T>
-class onLoading<Noting>:ResultStatus<Noting>
-data class Error<Nothing>(val errorResponse:ErrorResponse):ResultStatus<Nothing>
+class onLoading<T>:ResultStatus<T>
+data class Error<T>(val errorResponse:ErrorResponse):ResultStatus<T>
